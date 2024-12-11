676ae898e0f0f5bdeb90e01627131596f2b5c17c
RevanAga <revan.agazade.785@gmail.com>
2024-12-11 04:41:48 -0800

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Register from './register/Register'
 import Login from './login/Login'
+import HomePage from './homepage/HomePage'
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <Register/>
-    // <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
